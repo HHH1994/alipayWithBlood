400f9791322a30ca92b71a8a2b57b809efde5da0
HWS <15757135983@163.com>
2018-03-12 15:55:05 +0800

    项目初始化

---
+++ src/page/home.vue
@@ -0,0 +1,40 @@
+<style  lang="less" scoped>
+
+</style>
+<template>
+    <div class="home">
+      <tab>
+        <tab-item selected @on-item-click="onItemClick">已发货</tab-item>
+        <tab-item @on-item-click="onItemClick">未发货</tab-item>
+      </tab>
+    </div>
+</template>
+<script>
+  import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem } from 'vux'
+
+  export default {
+    components: {
+      Tab,
+      TabItem,
+      Sticky,
+      Divider,
+      XButton,
+      Swiper,
+      SwiperItem
+    },
+    data () {
+      return {
+        index: 0,
+        getBarWidth: function (index) {
+          return (index + 1) * 22 + 'px'
+        }
+      }
+    },
+    methods: {
+      onItemClick (index) {
+
+        console.log('on item click:', index)
+      }
+    }
+  }
+</script>
